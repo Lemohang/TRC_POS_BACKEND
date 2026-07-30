177e743287b3d8eecd8a2e63da355c57fbcf7355
Fuma1322 <leonardfuma26@gmail.com>
2026-07-30 17:47:37 +0000

    debug: CORS policy

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
-require("./dns-fix");
-require("dotenv").config();
+require('./dns-fix');
+require('dotenv').config();
 
-const app = require("./src/app");
-const connectDB = require("./src/config/db");
+const app = require('./src/app');
+const connectDB = require('./src/config/db');
 
 const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
+  await connectDB();
 
-    await connectDB();
-
-    app.listen(PORT, () => {
-        console.log("================================");
-        console.log(`Server running on Port ${PORT}`);
-        console.log(`Environment: ${process.env.NODE_ENV}`);
-        console.log("================================");
-    });
-
+  app.listen(PORT, () => {
+    console.log('================================');
+    console.log(`Server running on Port ${PORT}`);
+    console.log(`Environment: ${process.env.NODE_ENV}`);
+    console.log('================================');
+  });
 };
 
 startServer();
